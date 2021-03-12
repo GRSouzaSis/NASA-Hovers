c24642ac3ca0829fb577a99d526b5b172133d2c6
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2021-03-11 21:44:40 -0300

    refactor: move app file to src folder

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Text>Nasa</Text>
+    </>
+  );
+};
+
+export default App;
